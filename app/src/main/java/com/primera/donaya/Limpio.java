454b39dc2484b78e206f1353563a4ff55b66ff94
JavierLainez420 <101200751+JavierLainez420@users.noreply.github.com>
2022-11-16 16:07:37 -0600

    Logout

--- app/src/main/java/com/primera/donaya/Limpio.java
+++ app/src/main/java/com/primera/donaya/Limpio.java
@@ -1,15 +1,38 @@
 package com.primera.donaya;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Limpio extends AppCompatActivity {
 
     private TextView userNombre, userEmail, userID;
-    private ImageView userImg;
+    private CircleImageView userImg;
+    private FirebaseAuth mAuth;
+    Button btnCerrarSesion;
+
+    //Variables opcionales para desloguear de google tambien
+    private GoogleSignInClient mGoogleSignInClient;
+    private GoogleSignInOptions gso;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +43,51 @@ public class Limpio extends AppCompatActivity {
         userEmail = findViewById(R.id.userEmail);
         userID = findViewById(R.id.userId);
         userImg = findViewById(R.id.userImagen);
+        btnCerrarSesion = findViewById(R.id.btnLogout);
+
+    mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
 
 
 
+        //Establecer campos
+        userNombre.setText(currentUser.getDisplayName());
+        userEmail.setText(currentUser.getEmail());
+        userID.setText(currentUser.getUid());
+        Glide.with(this).load(currentUser.getPhotoUrl()).into(userImg);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+        btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Cerrar sesion con firebase
+                mAuth.signOut();
+
+                //Cerrar sesión con google tambien: Google sign out
+                mGoogleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        //Abrir MainActivity con SigIn button
+                        if(task.isSuccessful()){
+                            Intent mainActivity = new Intent(getApplicationContext(), Login.class);
+                            startActivity(mainActivity);
+                            Limpio.this.finish();
+                        }else{
+                            Toast.makeText(getApplicationContext(), "No se pudo cerrar sesión con google",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+
+            }
+        });
     }
 }
