34bf1926e6ca6d895db40c71242fc1404dd4bc25
JavierLainez420 <101200751+JavierLainez420@users.noreply.github.com>
2022-11-16 12:15:21 -0600

    Test Login google

--- app/src/main/java/com/primera/donaya/Login.java
+++ app/src/main/java/com/primera/donaya/Login.java
@@ -1,5 +1,6 @@
 package com.primera.donaya;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
@@ -11,13 +12,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class Login extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 1;
+    private static final String TAG = "GoogleSignInLoginActivity";
     TextView lblCrearCuenta;
     EditText txtInputEmail, txtInputPassword;
-    Button btnLogin;
+    Button btnLogin, btnGoogle;
     private ProgressDialog mProgressBar;
-
+    //Variable para gestionar FirebaseAuth
+    private FirebaseAuth mAuth;
+    //Agregar cliente de inicio de sesión de Google
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +52,7 @@ public class Login extends AppCompatActivity {
         txtInputPassword = findViewById(R.id.inputPassword);
         btnLogin = findViewById(R.id.btnlogin);
         lblCrearCuenta = findViewById(R.id.txtRegistratre);
+        btnGoogle = findViewById(R.id.btngoogle);
 
         lblCrearCuenta.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +68,90 @@ public class Login extends AppCompatActivity {
         });
 
         mProgressBar = new ProgressDialog(Login.this);
+
+        // Configurar Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    public void LoginGoogle(View view) {
+
+        signIn();
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Resultado devuelto al iniciar el Intent de GoogleSignInApi.getSignInIntent (...);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            if(task.isSuccessful()){
+                try {
+                    // Google Sign In was successful, authenticate with Firebase
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+                    Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                    firebaseAuthWithGoogle(account.getIdToken());
+                } catch (ApiException e) {
+                    // Google Sign In fallido, actualizar GUI
+                    Log.w(TAG, "Google sign in failed", e);
+                }
+            }else{
+                Log.d(TAG, "Error, login no exitoso:" + task.getException().toString());
+                Toast.makeText(this, "Ocurrio un error. "+task.getException().toString(),
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            //FirebaseUser user = mAuth.getCurrentUser();
+//Iniciar DASHBOARD u otra actividad luego del SigIn Exitoso
+
+                            Intent dashboardActivity = new Intent(Login.this, Limpio.class);
+                            startActivity(dashboardActivity);
+                            Login.this.finish();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+
+                        }
+                    }
+                });
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user!=null){ //si no es null el usuario ya esta logueado
+            //mover al usuario al dashboard
+            Intent dashboardActivity = new Intent(Login.this, Limpio.class);
+            startActivity(dashboardActivity);
+        }
+        super.onStart();
     }
 
     public void verificarCredenciales(){
@@ -80,6 +185,4 @@ public class Login extends AppCompatActivity {
 
 
 
-
-
-    }
+}
