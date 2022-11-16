5c66867ff9a82493644836d2c562a6b48e423e31
JavierLainez420 <101200751+JavierLainez420@users.noreply.github.com>
2022-11-16 12:16:26 -0600

    Test Login google

---
+++ app/src/main/java/com/primera/donaya/Limpio.java
@@ -0,0 +1,14 @@
+package com.primera.donaya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Limpio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_limpio);
+    }
+}
