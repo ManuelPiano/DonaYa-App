d0445d5306ad953eb2c252097e7a0400258cbd99
farygavidia <101201063+farygavidia@users.noreply.github.com>
2022-11-16 18:55:08 -0700

    logotipo

---
+++ app/src/main/java/com/primera/donaya/Mainlogotipo.java
@@ -0,0 +1,14 @@
+package com.primera.donaya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mainlogotipo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainlogotipo);
+    }
+}
